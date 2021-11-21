59fe24b2c86c31a706c36ab04d58715c137ad37c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2021-11-21 14:47:27 -0500

    check for round-trip in fuzzers
    suggested by Tej

--- rfc1057/fuzz.go
+++ rfc1057/fuzz.go
@@ -4,11 +4,16 @@
 package rfc1057
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/zeldovich/go-rpcgen/xdr"
 )
 
+type FUZZ_T = Rpc_msg
+
 func Fuzz(data []byte) int {
-	var x Rpc_msg
+	var x FUZZ_T
 	err := xdr.DecodeBuf(data, &x)
 	if err != nil {
 		return 0
@@ -17,6 +22,12 @@ func Fuzz(data []byte) int {
 	_, err = xdr.EncodeBuf(&x)
 	if err != nil {
 		panic(err)
+	}
+
+	var y FUZZ_T
+	xdr.DecodeBuf(data, &y)
+	if !reflect.DeepEqual(x, y) {
+		panic(fmt.Sprintf("%+v does not roundtrip; got %+v", x, y))
 	}
 
 	return 1
--- rfc1813/fuzz.go
+++ rfc1813/fuzz.go
@@ -4,12 +4,17 @@
 package rfc1813
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/zeldovich/go-rpcgen/xdr"
 )
 
+// type FUZZ_T = CREATE3args
+type FUZZ_T = READDIRPLUS3res
+
 func Fuzz(data []byte) int {
-	// var x CREATE3args
-	var x READDIRPLUS3res
+	var x FUZZ_T
 	err := xdr.DecodeBuf(data, &x)
 	if err != nil {
 		return 0
@@ -18,6 +23,12 @@ func Fuzz(data []byte) int {
 	_, err = xdr.EncodeBuf(&x)
 	if err != nil {
 		panic(err)
+	}
+
+	var y FUZZ_T
+	xdr.DecodeBuf(data, &y)
+	if !reflect.DeepEqual(x, y) {
+		panic(fmt.Sprintf("%+v does not roundtrip; got %+v", x, y))
 	}
 
 	return 1
