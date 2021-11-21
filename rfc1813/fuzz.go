4583362569e27e9f2bea084b8e82fb84100f6848
Nickolai Zeldovich <nickolai@csail.mit.edu>
2021-11-21 14:41:12 -0500

    checkpoint a fuzzing stub for rfc1813
    was able to reproduce the string memory use issue

---
+++ rfc1813/fuzz.go
@@ -0,0 +1,23 @@
+//go:build gofuzz
+
+package rfc1813
+
+import (
+	"github.com/zeldovich/go-rpcgen/xdr"
+)
+
+func Fuzz(data []byte) int {
+	// var x CREATE3args
+	var x READDIRPLUS3res
+	err := xdr.DecodeBuf(data, &x)
+	if err != nil {
+		return 0
+	}
+
+	_, err = xdr.EncodeBuf(&x)
+	if err != nil {
+		panic(err)
+	}
+
+	return 1
+}
